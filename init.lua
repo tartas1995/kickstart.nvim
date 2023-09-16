1a8d6ba1de54cfd834371559f07c9bf2a977147a
Mathis Kimmel <tartas1995@gmail.com>
2023-09-16 16:28:01 +0200

    init.lua tab chance

--- init.lua
+++ init.lua
@@ -535,5 +535,13 @@ cmp.setup {
   },
 }
 
+vim.cmd [[ 
+  set autoindent 
+  set expandtab 
+  set shiftwidth=4
+  set smartindent 
+  set softtabstop=4 
+  set tabstop=4 
+]]
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
